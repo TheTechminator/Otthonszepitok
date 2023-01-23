c3671aa83272bdb83ddfb3f742691cacc02160ee
TheTechminator <63601014+TheTechminator@users.noreply.github.com>
2023-01-23 18:15:47 +0100

    Add files via upload

---
+++ img/gallery/IMGCOUNT.php
@@ -0,0 +1,6 @@
+<?php
+	/* Meg lehet adni a képek számát amelyek megjelennek a galériában */
+	/* FONTOS: a képeket 1-töl számozzuk folyamatosan 1-esével (pl.: img1.jpg, img2.jpg) és minden képnek 'jpg' formátumban kell lennie */
+
+	$IMGCOUNT = 37; /* Ahány kép van a galériában */
+?>
